6a004c59ae20d8eb74962f071510f40cd247c52f
shumpei07 <shumpeikonno07@gmail.com>
2023-09-11 18:27:05 +0900

    プルリク用　微調整

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -4,11 +4,12 @@ require_once ROOT_PATH.'Controllers/Controller.php';
 require_once ROOT_PATH.'Controllers/UserController.php';
 
 
-class HomeController extends Controller
-{
-    public function index(){
+  class HomeController extends Controller
+  {
+    public function index()
+    {
       $user = new UserController();
       $auth = $user->getAuth();
       $this->view('home/index', ['auth' => $auth]);
     }
-}
+  }
